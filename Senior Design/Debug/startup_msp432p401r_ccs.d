af5841f3263686caaa5a41f2a79425adfdc2d1fa
Ryan Higgins <ryanjeffhiggins@gmail.com>
2018-06-15 12:35:52 -0500

    Crap Code

---
+++ Senior Design/Debug/startup_msp432p401r_ccs.d
@@ -0,0 +1,19 @@
+# FIXED
+
+startup_msp432p401r_ccs.obj: ../startup_msp432p401r_ccs.c
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h
+startup_msp432p401r_ccs.obj: /home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h
+
+../startup_msp432p401r_ccs.c: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h: 
+/home/ryhig/CCS/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h: 
